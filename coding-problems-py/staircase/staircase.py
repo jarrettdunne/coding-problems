4d453d9321b49299bd43c66e7f2e13a00ef50611
Jarrett <56049991+jarrettdunne@users.noreply.github.com>
2021-08-30 17:42:57 +0000

    Delete coding-problems-py directory

--- coding-problems-py/staircase/staircase.py
+++
@@ -1,8 +0,0 @@
-def staircase(n, X):
-    cache = [0 for _ in range(n + 1)]
-    cache[0] = 1
-    for i in range(1, n + 1):
-        cache[i] += sum(cache[i - x] for x in X if i - x >= 0)
-    return cache[n]
-
-print(staircase(4, [1, 2]))

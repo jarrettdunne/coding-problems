8eb45d929b2a5e33750605783bc06c9a55c78889
jarrett <jarrettpdunne@gmail.com>
2021-09-29 16:46:54 -0500

    create and write solution for hcf

---
+++ categorical/hcf.py
@@ -0,0 +1,19 @@
+#
+#
+#
+
+def hcf(a, b):
+    ''''''
+    if a > b:
+        smaller = b
+    else:
+        smaller = a
+
+    for i in range(1, smaller + 1):
+        if a % i == 0 and b % i == 0:
+            hcf = i
+
+    return hcf
+
+if __name__ == "__main__":
+    print(hcf(8, 15))

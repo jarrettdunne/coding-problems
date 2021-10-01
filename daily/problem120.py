478ffe97d5dc20158e98415dfbf3d114c3f6b958
jarrett <jarrettpdunne@gmail.com>
2021-09-30 22:18:21 -0500

    update problem120

--- daily/problem120.py
+++ daily/problem120.py
@@ -3,5 +3,21 @@
 # Implement the singleton pattern with a twist. 
 # First, instead of storing one instance, store two instances. 
 # And in every even call of getInstance(), return the first 
-# instance and in every odd call of getInstance(), return the second instance.
+#   instance and in every odd call of getInstance(), return the second instance.
 
+class Singleton:
+    __instance = None
+
+    def __new__(cls, *args):
+        if  cls.__instance is None:
+            cls.__instance = object.__new__(cls, *args)
+        return cls.__instance
+
+class DoubleSingleton:
+    __instance_1 = None
+    __instance_2 = None
+
+    def __new__(cls, *args):
+        ''''''
+        # need to rememebr call state
+        
